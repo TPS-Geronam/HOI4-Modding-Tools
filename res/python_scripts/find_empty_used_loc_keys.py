e1d521c9edffb26c3d7c36609da808b873936810
TPS-Geronam <29309465+TPS-Geronam@users.noreply.github.com>
2021-03-13 05:42:52 +0100

    Add files via upload

---
+++ res/python_scripts/find_empty_used_loc_keys.py
@@ -0,0 +1,20 @@
+import os
+import re
+#Goes through all the files in current directory, assumes script lies next to files
+dir = os.listdir(os.getcwd())
+#Create output file
+with open('__output.txt', 'w') as o:
+	for filename in dir:
+		#Check whether current file is not self, not output file and not a directory
+		if not os.path.isdir(filename) and filename != "find_empty_used_loc_keys.py" and filename != "__output.txt":
+			with open(filename, 'U') as f:
+				lines = f.read().splitlines()
+				reg = re.compile("(:(.) *\"( *)\")")
+				for l in lines:
+					if reg.findall(l) != []:
+						o.write(l + '\n')
+						print(l)
+		#File is self
+		else:
+			print "Encountered self, skipping."
+input("Press enter to exit the script.")
