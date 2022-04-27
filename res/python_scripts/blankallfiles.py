21e7230aea391a3206cd3b3f0e603c44fa92546f
TPS-Geronam <29309465+TPS-Geronam@users.noreply.github.com>
2022-04-28 01:10:35 +0200

    Update blankallfiles.py

--- res/python_scripts/blankallfiles.py
+++ res/python_scripts/blankallfiles.py
@@ -4,10 +4,10 @@ def main():
 	dir = os.listdir(os.getcwd())
 	for filename in dir:
 		if filename != "blankallfiles.py":
-			with open(filename, "w") as f
+			with open(filename, "w") as f:
 				f.write("")
 		else:
-			print "Encountered self, skipping."
+			print("Encountered self, skipping.")
 
 if __name__ == "__main__":
 	main()
