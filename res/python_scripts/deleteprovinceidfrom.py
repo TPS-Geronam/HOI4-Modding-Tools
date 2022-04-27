bacd8ff318af118da3868aeb0bb6f4e088e6ea38
TPS-Geronam <29309465+TPS-Geronam@users.noreply.github.com>
2022-04-28 01:27:08 +0200

    Update deleteprovinceidfrom.py

--- res/python_scripts/deleteprovinceidfrom.py
+++ res/python_scripts/deleteprovinceidfrom.py
@@ -1,14 +1,14 @@
 def main():
-	x = raw_input("Province ID's seperated by spaces: ")
-	#Seperation char
-	data = x.split(' ')
+	x = input("Province IDs separated by spaces: ")
+	#Separation char
+	data = x.split(" ")
 	for id in data:
-	  	#Source file, assumes that script lies next to source file
-	  	#Deletes given ID's from source file
-		with open("1-World.txt", 'r') as f:
+		#Source file, assumes that script lies next to source file
+		#Deletes given ID's from source file
+		with open("1-World.txt", "r") as f:
 			#Assumes Province ID's are present in the ' 1234 5678 91011 ' format (every ID has a space in front and behind of itself)
 			newText = f.read().replace(f" {id} ", " ")
-	  	with open('1-World.txt', "w") as f:
+		with open("1-World.txt", "w") as f:
 			f.write(newText)
 
 if __name__ == "__main__":
