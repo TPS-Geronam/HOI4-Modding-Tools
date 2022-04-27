3f0088e7d9bc84c21a5b168ebe5d369884e8be21
TPS-Geronam <29309465+TPS-Geronam@users.noreply.github.com>
2022-04-28 01:25:36 +0200

    Update copyflagdummy.py

--- res/python_scripts/copyflagdummy.py
+++ res/python_scripts/copyflagdummy.py
@@ -2,14 +2,15 @@ import os
 import shutil
 
 def main():
-	x = raw_input("TAG's seperated by spaces: ")
-	#Seperation char
-	data = x.split(' ')
+	tags = input("TAGs separated by spaces: ")
+	#Separation char
+	data = tags.split(" ")
 	#Name of dummy flag file
-	srcfile = os.getcwd() + "\dummy.tga"
+	srcfile = "./dummy.tga"
 	for tag in data:
-		desfile = f"{os.getcwd()}\\{tag}.tga"
-		shutil.copy(srcfile, desfile
+		desfile = f"./{tag}.tga"
+		shutil.copy(srcfile, desfile)
 
 if __name__ == "__main__":
 	main()
+	
